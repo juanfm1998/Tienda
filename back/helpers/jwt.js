e9fa34f847f17f51a4bce94f4a0c5c0b4e5198aa
juanfm1998 <juanfm154@gmail.com>
2021-12-13 21:20:04 -0500

    admin

---
+++ back/helpers/jwt.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var jwt=require('jwt-simple');
+
+var moment=require('moment');
+
+var secret='maxpayne'
+
+exports.createToken=function(user){
+    var payload={
+        
+    }
+}
